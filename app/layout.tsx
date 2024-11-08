d53eedfb37d2dbd96a5255e6608601c45ba86c01
Dilip Kumar <indla-dilip.kumar@capgemini.com>
2024-11-08 10:12:09 +0000

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             <Header />
             {children}
             <Footer />
+            <Analytics/>
           </div>
         </ThemeProvider>
       </body>
